077bf5280d01b3b7d54ceeb02a5b1ba1ce59bac7
j3ager-1lyas <ilyas.elasri.unreal@gmail.com>
2023-10-27 19:09:23 +0100

    Animation [ for Buttons] Correction [Slide Initial Position]

--- apps/frontend/script.js
+++ apps/frontend/script.js
@@ -1,9 +1,10 @@
 /** Styling the BarSlide Script */
 const slideValue=document.querySelector("span");
 const inputSlider= document.querySelector("#gridSize");
+slideValue.style.left = (16) + '%';
 inputSlider.oninput = (()=>{
     let value = inputSlider.value;
     slideValue.textContent = value;
     slideValue.style.left = (value) + '%';
-
 })
+
